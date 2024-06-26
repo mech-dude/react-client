87bb8d897bb02f33d9d364f55911c8c6517897a1
Juan <juan.sebastian4597@hotmail.com>
2024-06-26 16:47:46 -0500

    edited websocket client config

--- src/websocket/websocket_client.js
+++ src/websocket/websocket_client.js
@@ -10,12 +10,32 @@ function getWebSocketUrl() {
   if (!REACT_APP_NODE_ENV || !wsURLs[REACT_APP_NODE_ENV]) {
     throw new Error("Unknown environment");
   }
-
   return wsURLs[REACT_APP_NODE_ENV];
 }
 
-console.log(getWebSocketUrl());
+console.log("WebSocket URL:", getWebSocketUrl());
 export const socket = io(getWebSocketUrl(), {
-    autoConnect: true,
-    transports: ["websocket"]
+  autoConnect: true,
+  transports: ["websocket"],
+  upgrade: false
+});
+
+socket.on('connect', () => {
+  console.log('Successfully connected to the WebSocket server');
+  const transport = socket.io.engine.transport.name; // in most cases, "polling"
+
+  socket.io.engine.on("upgrade", () => {
+    const upgradedTransport = socket.io.engine.transport.name; // in most cases, "websocket"
+  });
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from the WebSocket server');
+});
+
+socket.on('connect_error', (err) => {
+  console.error('Connection error:', err);
+  console.log(err.code);
+  console.log(err.message); 
+  console.log(err.context);
 });
